529a1e3711fe8b00da9ff34f1e774834c4805020
lakshmanan <lakshmananvcetcse@gmail.com>
2019-06-08 08:00:49 +0530

    first commit

---
+++ src/main/java/primeNumberSieves/prime_sieves/App.java
@@ -0,0 +1,13 @@
+package primeNumberSieves.prime_sieves;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
